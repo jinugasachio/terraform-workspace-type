78679296c04ef36a9ce2e89e3bbdda7172b19b97
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-26 14:09:36 +0900

    workspaceへ渡すためにリダイレクト

--- scripts/circleci/assume_role.sh
+++ scripts/circleci/assume_role.sh
@@ -9,8 +9,4 @@ aws_sts_credentials="$(aws sts assume-role \
   --query "Credentials" \
   --output "json")"
 
-cat <<EOT > "aws_sts_credentials.sh"
-echo 'export AWS_ACCESS_KEY_ID=$(echo $aws_sts_credentials | jq -r '.AccessKeyId')' >> $BASH_ENV
-echo 'export AWS_SECRET_ACCESS_KEY=$(echo $aws_sts_credentials | jq -r '.SecretAccessKey')' >> $BASH_ENV
-echo 'export AWS_SESSION_TOKEN=$(echo $aws_sts_credentials | jq -r '.SessionToken')' >> $BASH_ENV
-EOT
+echo $aws_sts_credentials >> /tmp/aws_sts_credentials.json
