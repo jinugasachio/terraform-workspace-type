9226dc778913c1f0b4d89a016b70b0d116888339
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-26 19:20:18 +0900

    tflintで2つとも検出されることを確認したのでコメントアウト

--- sample/sub/main.tf
+++ sample/sub/main.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "sample_sub_bucket" {
 }
 
 # tflint 検知用
-resource "aws_instance" "foo" {
-  ami           = "ami-0ff8a91507f77f867"
-  instance_type = "t3.small"
-}
+// resource "aws_instance" "goo" {
+//   ami           = "ami" invalid AMI ID. (aws_instance_invalid_ami)
+//   instance_type = "t3.small"
+// }
