9dbde584b2a6cc150c1efba6dc08e3e5c617d87e
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-26 21:21:53 +0900

    plan実行用のスクリプト追加

---
+++ scripts/circleci/terraform_plan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eo pipefail
+TARGET_DIRS=$@
+
+for dir in $TARGET_DIRS
+do
+  cd $CIRCLE_WORKING_DIRECTORY/$dir
+  NUMBER_OF_TF_FILES=$(find ./*.tf -type f | wc -l) # この判定は微妙。terraform.tf決めうちで良いかもしれない
+
+  if [ $NUMBER_OF_TF_FILES -gt 0 ]; then
+    tfcmt --config $CIRCLE_WORKING_DIRECTORY/tfcmt.yaml -var target:$dir plan -- terraform plan -no-color
+  fi
+done
