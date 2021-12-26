4b2344dba356bcf12bf89370193838f58844c956
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-26 18:30:00 +0900

    スクリプト修正

--- scripts/circleci/ tflint.sh
+++ scripts/circleci/ tflint.sh
@@ -1,5 +1,6 @@
-
 #!/bin/bash
+
+set -eo pipefail
 
 TARGET_DIRS=$@
 echo $TARGET_DIRS | xargs -r -n 1 bash -c 'cd $CIRCLE_WORKING_DIRECTORY/$0 && tflint --config $CIRCLE_WORKING_DIRECTORY/.tflint.hcl --format compact'
