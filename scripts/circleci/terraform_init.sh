a87c99f85f9e2563a5ea355e208381819fdd82e9
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-26 22:26:35 +0900

    指定方法を変更

--- scripts/circleci/terraform_init.sh
+++ scripts/circleci/terraform_init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -eo pipefail
+
 TARGET_DIRS=$@
+CONFIG_FILE="terraform.tf"
 
 for dir in $TARGET_DIRS
 do
   cd $CIRCLE_WORKING_DIRECTORY/$dir
-  NUMBER_OF_TF_FILES=$(find ./*.tf -type f | wc -l) # この判定は微妙。terraform.tf決めうちで良いかもしれない
-
-  if [ $NUMBER_OF_TF_FILES -gt 0 ]; then
+  if [ -e $CONFIG_FILE ]; then
     terraform init -input=false -no-color
   fi
 done
