95a68052921b264c5f74acd0dd0dde2c11600841
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-26 18:24:26 +0900

    initはルートで一回やれば良いっぽい

--- scripts/circleci/tflint_init.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-
-set -eo pipefail
-
-TARGET_DIRS=$@
-echo $TARGET_DIRS | xargs -r -n 1 bash -c 'cd $CIRCLE_WORKING_DIRECTORY/$0 && tflint --init --config $CIRCLE_WORKING_DIRECTORY/.tflint.hcl'
