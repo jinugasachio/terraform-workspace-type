3e49d22ba423d09fa60e3632c5c75abf7c0b82dc
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-26 17:04:32 +0900

    スクリプト追加

---
+++ scripts/circleci/detect_changed_dirs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# 差分のあるファイルのディレクトリを一意に取得する
+git --no-pager diff origin/main..HEAD --name-only  | xargs -I{} dirname {} | awk '!a[$0]++{print}'
+
+# TODO: main直pushだと差分を検出できないので条件分岐で処理を書く必要がある
+#       https://blog.hatappi.me/entry/2018/10/08/232625
