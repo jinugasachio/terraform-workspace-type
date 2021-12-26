d7ebe64dbe9fe1f4249762f38c680c82267af384
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-26 21:30:22 +0900

    tfcmtの設定ファイル追加

--- scripts/circleci/terraform_plan.sh
+++ scripts/circleci/terraform_plan.sh
@@ -9,6 +9,6 @@ do
   NUMBER_OF_TF_FILES=$(find ./*.tf -type f | wc -l) # この判定は微妙。terraform.tf決めうちで良いかもしれない
 
   if [ $NUMBER_OF_TF_FILES -gt 0 ]; then
-    tfcmt --config $CIRCLE_WORKING_DIRECTORY/tfcmt.yaml -var target:$dir plan -- terraform plan -no-color
+    tfcmt --config $CIRCLE_WORKING_DIRECTORY/tfcmt.yml -var target:$dir plan -- terraform plan -no-color
   fi
 done
