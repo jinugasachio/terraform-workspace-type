87924dfb55821dd8dc41df2368c0f9f1c425c586
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-29 19:25:04 +0900

    ciでprをマージする

--- scripts/circleci/merge_pr.sh
+++ scripts/circleci/merge_pr.sh
@@ -2,3 +2,11 @@
 
 set -euo pipefail
 
+PULL_REQUEST_NUMBER=${CIRCLE_PULL_REQUEST##*/}
+
+curl \
+  -X PUT \
+  -H "Accept: application/vnd.github.v3+json" \
+  -u $CIRCLE_PROJECT_USERNAME:$GITHUB_TOKEN \
+  -d '{"sha":"${CIRCLE_SHA1}"}'
+  https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$PULL_REQUEST_NUMBER/merge \
