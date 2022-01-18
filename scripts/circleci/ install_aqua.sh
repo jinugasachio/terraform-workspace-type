89e95369ba4341a3848a3e510be0216504478969
Yukio Ugajin <jinugasachio@gmail.com>
2022-01-18 21:43:33 +0900

    aquaをインストールするスクリプト追加

--- scripts/circleci/ install_aqua.sh
+++ scripts/circleci/ install_aqua.sh
@@ -2,4 +2,5 @@
 
 set -euo pipefail
 
-curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v0.6.0/aqua-installer | bash -s -- -v v0.10.0
+# install latest version
+curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v0.6.0/aqua-installer | bash
