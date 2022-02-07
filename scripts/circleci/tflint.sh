93500068e68d08b5f9d3d29a8777341bbbf2f162
Yukio Ugajin <jinugasachio@gmail.com>
2022-02-07 20:49:41 +0900

    reviewdogテスト

--- scripts/circleci/tflint.sh
+++ scripts/circleci/tflint.sh
@@ -3,4 +3,5 @@
 set -euo pipefail
 
 TARGET_DIRS=$@
-echo $TARGET_DIRS | xargs -r -n 1 bash -c 'cd $CIRCLE_WORKING_DIRECTORY/$0 && tflint --config $CIRCLE_WORKING_DIRECTORY/.tflint.hcl --format compact'
+export REVIEWDOG_GITHUB_API_TOKEN=$GITHUB_TOKEN
+echo $TARGET_DIRS | xargs -r -n 1 bash -c 'cd $CIRCLE_WORKING_DIRECTORY/$0 && tflint --config $CIRCLE_WORKING_DIRECTORY/.tflint.hcl --format compact | reviewdog -reporter=github-pr-review'
